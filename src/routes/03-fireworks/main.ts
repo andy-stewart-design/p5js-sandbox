7d3149f3d2076fe08a8d98c4c5c2f6a882841eeb
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-11-22 08:38:29 -0500

    Making firework loop more efficient

--- src/routes/03-fireworks/main.ts
+++ src/routes/03-fireworks/main.ts
@@ -27,19 +27,17 @@ function init() {
 
     p.draw = function draw() {
       p.background(0);
-      for (let i = 0; i < circles.length; i++) {
+
+      for (let i = circles.length - 1; i >= 0; i--) {
         const circle = circles[i];
-        circle.o -= 6;
+
+        if (circle.o - 6 < 0) circles.splice(i, 1);
+        else circle.o -= 6;
+
         circle.y += circle.vY;
         circle.x += circle.vX;
         circle.vX *= 0.85;
         circle.vY *= 0.85;
-        // const colorSelector = Math.random();
-        // if (colorSelector >= 0 && colorSelector < 0.33)
-        //   p.fill(0, 0, 255, circle.o);
-        // else if (colorSelector >= 0.33 && colorSelector < 0.66)
-        //   p.fill(255, 0, 0, circle.o);
-        // else p.fill(255, 255, 255, circle.o);
 
         const [r, g, b] = circle.colors;
         p.fill(r, g, b, circle.o);
@@ -47,7 +45,7 @@ function init() {
         p.ellipse(circle.x, circle.y, circle.r);
       }
 
-      circles = circles.filter((c) => c.o > 0);
+      console.log(circles.length);
     };
 
     p.mouseClicked = function mouseClicked() {
@@ -64,7 +62,6 @@ function init() {
         const directionY = Math.random() - 0.5 >= 0 ? 1 : -1;
 
         const colorSelector = Math.random();
-        console.log(colorSelector);
 
         const colorArray =
           colorSelector >= 0 && colorSelector < 0.33
